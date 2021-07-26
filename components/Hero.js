31274ed539ddf9923d32a4098c01450c0b34b7b0
qt_coder <65493032+qt-coder@users.noreply.github.com>
2021-07-26 14:51:18 +0800

    Added hero section

---
+++ components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <div className='hero'>
+            <h1 className="title">QT Coder's Blog</h1>
+            <p className="desc">Hi! I'm qt, a highschool student and aspiring software developer. <br/> I occassionally make blog posts about my projects or frontend development.</p>
+            <a className="cta" href='#posts'>Check it out</a>
+        </div>
+    )
+}
+
+export default Hero
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import path from 'path'
 import matter from 'gray-matter'
 import Post from '../components/Post'
 import styles from '../styles/Home.module.css'
+import Hero from '../components/Hero'
 
 export default function Home({posts}) {
   return (
@@ -14,7 +15,10 @@ export default function Home({posts}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="posts">
+      <Hero/>
+
+      <div id="posts">
+        <h1 id='featured'>Posts</h1>
         {posts.map((post, index) => (
           <Post post={post} key={index}/>
         ))}
